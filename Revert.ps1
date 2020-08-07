db507dcca9ddaefe2972d0b469eb94835fe23650
mrapte <67079953+mrapte@users.noreply.github.com>
2020-08-06 20:46:56 -0700

    Added Messages

--- Revert.ps1
+++ Revert.ps1
@@ -11,26 +11,41 @@ $ESXiDisplayName = "pacific-esxi-7"
 $NSXTMgrDisplayName = "pacific-nsx-3"
 $NSXTEdgeDisplayName = "pacific-nsx-edge-3a"
 
+echo "Connecting to $VIServer"
 $viConnection = Connect-VIServer $VIServer -User $VIUsername -Password $VIPassword -WarningAction SilentlyContinue
 
+echo "Reverting ESXi"
 $VMtoRevertESXi = Get-VM -name $ESXiDisplayName -Server $viConnection
 Set-VM -VM $VMtoRevertESXi -Snapshot "Nested-NSX-T-3.0-Base" -Confirm:$false
 
+echo "Reverting VCSA"
 $VMtoRevertVCSA = Get-VM -name $VCSADisplayName -Server $viConnection
 Set-VM -VM $VMtoRevertVCSA -Snapshot "Nested-NSX-T-3.0-Base" -Confirm:$false
 
+echo "Reverting NSXTMgr"
 $VMtoRevertNSXTMgr = Get-VM -name $NSXTMgrDisplayName -Server $viConnection
 Set-VM -VM $VMtoRevertNSXTMgr -Snapshot "Nested-NSX-T-3.0-Base" -Confirm:$false
 
+echo "Reverting NSXTEdge"
 $VMtoRevertNSXTEdge = Get-VM -name $NSXTEdgeDisplayName -Server $viConnection
 Set-VM -VM $VMtoRevertNSXTEdge -Snapshot "Nested-NSX-T-3.0-Base" -Confirm:$false
 
 if ($args[0] -eq 1) {
+    echo "Starting ESXi"
     Start-VM -VM $VMtoRevertESXi
+    echo "Sleeping 2 mins"
     Start-Sleep -Seconds 120
+
+    echo "Starting VCSA"
     Start-VM -VM $VMtoRevertVCSA
+    echo "Sleeping 4 mins"
     Start-Sleep -Seconds 240
+
+    echo "Starting NSXTMgr"
     Start-VM -VM $VMtoRevertNSXTMgr
+    echo "Sleeping 2 mins"
     Start-Sleep -Seconds 120
+
+    echo "Starting NSXTEdge"
     Start-VM -VM $VMtoRevertNSXTEdge
 }
