a2f5024318920370468551d8e8e33308d3ab7fb0
mrapte <67079953+mrapte@users.noreply.github.com>
2020-08-09 17:06:36 -0700

    Added PowerOnNested Script

--- PowerOnNested.ps1
+++ PowerOnNested.ps1
@@ -6,16 +6,12 @@ $VIServer = "10.0.5.8"
 $VIUsername = "administrator@vsphere.local"
 $VIPassword = "Viloha123!"
 
-$VMsToWorkOn = "pacific-vcsa-3", "pacific-esxi-7", "pacific-nsx-3", "pacific-nsx-edge-3a"
+$VMsToWorkOn = "pacific-esxi-7", "pacific-vcsa-3", "pacific-nsx-3", "pacific-nsx-edge-3a"
 
 echo "Connecting to $VIServer"
 $viConnection = Connect-VIServer $VIServer -User $VIUsername -Password $VIPassword -WarningAction SilentlyContinue
 
 $VMsToWorkOn.GetEnumerator() | Foreach-Object {
-    echo "Reverting $_"
-    Set-VM -VM (Get-VM -name $_ -Server $viConnection) -Snapshot "Nested-NSX-T-3.0-Base" -Confirm:$false
-}
-
-if ($args[0] -eq 1) {
-    .\PowerOnNested.ps1
+    echo "Starting $_"
+    Start-VM -VM (Get-VM -name $_ -Server $viConnection)
 }
