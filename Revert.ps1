f80e5d038bba0333986cff886a8186015bebd724
mrapte <67079953+mrapte@users.noreply.github.com>
2020-08-15 14:22:32 -0700

    Updates

--- Revert.ps1
+++ Revert.ps1
@@ -1,6 +1,14 @@
 # Author: 
 # Website: 
 
+# Snapshot name = Nested-NSX-T-3.0-Workload, Nested-NSX-T-3.0-Base
+if ($args.Count -ne 2) {
+    echo "Invalid Parameters"
+    exit
+}
+
+$SnapShotName = $args[0]
+
 # vCenter Server used to deploy vSphere with Kubernetes Lab
 $VIServer = "10.0.5.8"
 $VIUsername = "administrator@vsphere.local"
@@ -12,10 +20,10 @@ echo "Connecting to $VIServer"
 $viConnection = Connect-VIServer $VIServer -User $VIUsername -Password $VIPassword -WarningAction SilentlyContinue
 
 $VMsToWorkOn.GetEnumerator() | Foreach-Object {
-    echo "Reverting $_"
-    Set-VM -VM (Get-VM -name $_ -Server $viConnection) -Snapshot "Nested-NSX-T-3.0-Base" -Confirm:$false
+    echo "Reverting VM $_ to snapshot name $SnapShotName"
+    Set-VM -VM (Get-VM -name $_ -Server $viConnection) -Snapshot $SnapShotName -Confirm:$false
 }
 
-if ($args[0] -eq 1) {
+if ($args[1] -eq 1) {
     .\PowerOnNested.ps1
 }
